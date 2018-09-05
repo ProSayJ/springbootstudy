6b24534e6fc6359734a73e798ae41afa43cd0fe7
yangjian <yangjian@bubi.cn>
2018-09-05 18:18:12 +0800

    提交一个文件

---
+++ javase/src/main/java/com/prosayj/springboot/javase/base/day03_baseoperator/code/_00BreakDemo.java
@@ -0,0 +1,19 @@
+package com.prosayj.springboot.javase.base.day03_baseoperator.code;
+
+/*
+break 关键字
+作用于循环中,终止循环的作用
+*/
+public class _00BreakDemo {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 2000) {
+            if (i == 3) {
+                break;
+            } else {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
