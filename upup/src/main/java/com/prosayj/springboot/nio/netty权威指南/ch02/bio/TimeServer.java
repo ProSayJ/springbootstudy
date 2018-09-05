43247297536b3022c3f9de46e23cad1741180ce4
yangjian <yangjian@bubi.cn>
2018-09-05 19:36:45 +0800

    Feat:upup模块新增nio学习代码

---
+++ upup/src/main/java/com/prosayj/springboot/nio/netty权威指南/ch02/bio/TimeServer.java
@@ -0,0 +1,47 @@
+package com.prosayj.springboot.nio.netty权威指南.ch02.bio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author yangjian
+ * @description
+ * @Date 12:01 2018/8/20
+ * @since 1.0.0
+ */
+public class TimeServer {
+
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // 采用默认值
+            }
+
+        }
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(port);
+            System.out.println("The time server is start in port : " + port);
+            Socket socket = null;
+            while (true) {
+                socket = server.accept();
+                new Thread(new TimeServerHandler(socket)).start();
+            }
+        } finally {
+            if (server != null) {
+                System.out.println("The time server close");
+                server.close();
+                server = null;
+            }
+        }
+    }
+}
