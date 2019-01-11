9c3a726f807948bd8803b071872329e672e7b6cd
yangjian <yangjian@bubi.cn>
2019-01-11 16:16:49 +0800

    Refactor:新增重新注解

--- utils/src/main/java/com/prosayj/springboot/utils/easycaptchautil/servlet/CaptchaServlet.java
+++ utils/src/main/java/com/prosayj/springboot/utils/easycaptchautil/servlet/CaptchaServlet.java
@@ -20,11 +20,13 @@ import com.prosayj.springboot.utils.easycaptchautil.utils.CaptchaUtil;
 public class CaptchaServlet extends HttpServlet {
     private static final long serialVersionUID = -90304944339413093L;
 
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         CaptchaUtil.out(request, response);
     }
 
+    @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         doGet(request, response);
