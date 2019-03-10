c10ec7164fa68e36e1dc6b40fc18d981373c4c78
yang jian <prosayj@gmail.com>
2019-03-11 01:16:16 +0800

    Refactor:myblog模块启动的时候不注册数据源

--- myblog/src/main/java/com/prosayj/springboot/MyblogApplication.java
+++ myblog/src/main/java/com/prosayj/springboot/MyblogApplication.java
@@ -1,9 +1,18 @@
 package com.prosayj.springboot;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        DruidDataSourceAutoConfigure.class,
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class MyblogApplication {
 
     public static void main(String[] args) {
