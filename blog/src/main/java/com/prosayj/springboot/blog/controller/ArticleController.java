d00963b1f739578e72de9ca2de598e947c40c56a
yangjian <yangjian@bubi.cn>
2019-03-13 18:03:13 +0800

    Refactor:blog模块代码规范整理

--- blog/src/main/java/com/prosayj/springboot/blog/controller/ArticleController.java
+++ blog/src/main/java/com/prosayj/springboot/blog/controller/ArticleController.java
@@ -1,7 +1,14 @@
 package com.prosayj.springboot.blog.controller;
 
+import com.prosayj.springboot.blog.controller.vo.Blogs;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author yangjian
@@ -15,5 +22,18 @@ public class ArticleController {
     @GetMapping("/editor")
     public String login() {
         return "editor";
+    }
+
+    @PostMapping("/publishArticle")
+    @ResponseBody
+    public Map<String, String> publishArticle(Blogs blogs, HttpServletRequest request) {
+        String mdArticleContent = blogs.getArticleContent();
+        System.out.println(mdArticleContent);
+        //获得文章html代码并生成摘要
+        String articleHtmlContent = blogs.getArticleHtmlContent();
+        System.out.println(articleHtmlContent);
+        Map<String, String> result = new HashMap<>();
+        result.put("status", "200");
+        return result;
     }
 }
