bfb3cb2230c98b5d74bb9df7f6c2a1249b30a614
yang jian <prosayj@gmail.com>
2019-03-14 23:02:06 +0800

    Refactor:更新swagger2接口版本到2.8.0 、增加spring拦截器utf-8编码规则

--- blog/src/main/java/com/prosayj/springboot/blog/api/HelloController.java
+++ blog/src/main/java/com/prosayj/springboot/blog/api/HelloController.java
@@ -1,5 +1,6 @@
 package com.prosayj.springboot.blog.api;
 
+import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @creatTime 2019/3/11 13:28
  * @since 1.0.0
  */
+@Api(value = "hello-controller", tags = "hello-controller", description = "测试类")
 @Controller
 //@RestController
 public class HelloController {
--- blog/src/main/java/com/prosayj/springboot/config/Swagger2.java
+++ blog/src/main/java/com/prosayj/springboot/config/Swagger2.java
@@ -9,6 +9,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author yangjian
@@ -18,6 +19,7 @@ import springfox.documentation.spring.web.plugins.Docket;
  * @since 1.0.0
  */
 @Configuration
+@EnableSwagger2
 public class Swagger2 {
     @Bean
     public Docket createRestApi() {
