766c9b4de1908e11227e066581b32311c2f2342f
yang jian <prosayj@gmail.com>
2019-03-29 23:32:14 +0800

    Feat：新增开源博客代码：https://github.com/halo-dev/halo/releases/tag/v0.4.3

---
+++ halo/src/main/java/com/prosayj/springboot/halo/service/GalleryService.java
@@ -0,0 +1,16 @@
+package com.prosayj.springboot.halo.service;
+
+import com.prosayj.springboot.halo.model.domain.Gallery;
+import com.prosayj.springboot.halo.service.base.CrudService;
+
+/**
+ * <pre>
+ *     图库业务逻辑接口
+ * </pre>
+ *
+ * @author : ProSayJ
+ * @date : 2018/2/26
+ */
+public interface GalleryService extends CrudService<Gallery, Long> {
+
+}
