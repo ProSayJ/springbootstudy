552aaa51bcee23ded64f75d9889cf63ab43b1364
yangjian <yangjian@bubi.cn>
2019-07-04 19:22:11 +0800

    Feat:新增笔记

---
+++ upup/src/main/java/com/prosayj/springboot/Test/AnnotionOperator.java
@@ -0,0 +1,26 @@
+package com.prosayj.springboot.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @author yangjian
+ * @description TODO
+ * @email yangjian@bubi.cn
+ * @creatTime 2019/7/4 上午 09:57
+ * @since 1.0.0
+ */
+public class AnnotionOperator {
+    public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        AttachEmlement element = new AttachEmlement(); // 初始化一个实例，用于方法调用
+
+        Method[] methods = AttachEmlement.class.getDeclaredMethods(); // 获得所有方法
+        for (Method method : methods) {
+            AttachAnnotation annotationTmp = null;
+            if ((annotationTmp = method.getAnnotation(AttachAnnotation.class)) != null)
+                method.invoke(element, annotationTmp.paramValue());
+            else
+                method.invoke(element, "新的设置的省");
+        }
+    }
+}
---
+++ upup/src/main/java/com/prosayj/springboot/Test/AttachAnnotation.java
@@ -0,0 +1,19 @@
+package com.prosayj.springboot.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author yangjian
+ * @description TODO
+ * @email yangjian@bubi.cn
+ * @creatTime 2019/7/4 上午 09:56
+ * @since 1.0.0
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface AttachAnnotation {
+    String paramValue() default "默认省"; // 参数名为"paramValue" 默认值为"河北省"
+}
